09904d7d6f6f0dbe61450e5557258f9b36cc6865
unknown <carlosmorenobarrado@gmail.com>
2025-07-06 13:55:16 +0200

    Primer despliegue

--- telegram_scraper/app.py
+++ telegram_scraper/app.py
@@ -40,7 +40,7 @@ SESSION_NAME = 'my_telegram_session' # Manteniendo el nombre de sesión solicita
 TARGET_GROUP_ID = -1002383848783 # ¡Asegúrate de que este ID es el correcto para tu supergrupo!
 TARGET_TOPIC_NAME = "IDEAS CORTO PLAZO/AT"
 
-logging.info("Cargadas variables")
+logging.info("Cargadas vars")
 
 # Función auxiliar para serializar objetos datetime y bytes a JSON
 def json_serial(obj):
